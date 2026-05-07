411888a71eec710bc146468925603d40527c8971
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 18:16:45 -0300

    feat: add DesativaUsuarioRequest para desativacao do usuario

---
+++ src/Fcg.Usuarios.Application/Dtos/DesativaUsuarioRequest.cs
@@ -0,0 +1,20 @@
+﻿using Fcg.Usuarios.Domain.Enum;
+namespace Fcg.Usuarios.Application.Dtos
+{
+    public class DesativaUsuarioRequest
+    {
+        public Guid Id { get; set; }
+        public MotivoDesativacao MotivoDelecao { get; set; }
+
+        public DesativaUsuarioRequest()
+        {            
+        }
+
+        public DesativaUsuarioRequest(Guid id, MotivoDesativacao motivoDelecao)
+        {
+            Id = id;
+            MotivoDelecao = motivoDelecao;
+        }
+    }
+        
+}
