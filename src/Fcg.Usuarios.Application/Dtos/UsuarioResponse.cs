357301090c325466bd3e77dcb00aabe3a25b86ad
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 18:19:36 -0300

    feat: add UsuarioResponse para retorno de dados do usuario para o Administrador verificar

---
+++ src/Fcg.Usuarios.Application/Dtos/UsuarioResponse.cs
@@ -0,0 +1,15 @@
+﻿using Fcg.Usuarios.Domain.Enum;
+
+namespace Fcg.Usuarios.Application.Dtos
+{
+    public class UsuarioResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+        public TipoUsuario PerfilUsuario {  get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public MotivoDesativacao? MotivoDesativacao { get; private set; }
+    }
+}
