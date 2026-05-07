78358cd43c860584dd3c3895db6fad7c0c311caf
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 17:58:02 -0300

    feat: add IAggreateRoot para Interfaces

---
+++ src/Fcg.Usuarios.Domain/Common/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fcg.Usuarios.Domain.Common.Interfaces
+{
+    public interface IAggregateRoot
+    {    }
+}
