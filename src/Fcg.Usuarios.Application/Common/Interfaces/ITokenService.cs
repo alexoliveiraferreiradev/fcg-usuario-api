4288a2a3a5e909fed283500a8268bfdcc4704837
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:59:58 -0300

    refactor: relocate token service to infrastructure and add clean architecture references

---
+++ src/Fcg.Usuarios.Application/Common/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using Fcg.Usuarios.Application.Features.Token;
+using Fcg.Usuarios.Application.Features.Usuarios;
+
+namespace Fcg.Usuarios.Application.Common.Interfaces
+{
+    public interface ITokenService
+    {
+        Task<TokenResult> GerarToken(UsuarioResponse usuario);
+    }
+}
---
+++ src/Fcg.Usuarios.Application/Features/Token/TokenResult.cs
@@ -0,0 +1,11 @@
+﻿using Fcg.Usuarios.Application.Dtos;
+
+namespace Fcg.Usuarios.Application.Features.Token
+{
+    public class TokenResult
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public IEnumerable<ClaimResponse> Claims { get; set; }
+    }
+}
