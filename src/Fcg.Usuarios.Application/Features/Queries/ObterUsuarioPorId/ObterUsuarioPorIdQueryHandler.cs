1a27aa2a45722da9f9420dc9be7bc3d5473a2926
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:08:04 -0300

    feat(application): adiciona ObterUsuarioPorIdQueryHandler

---
+++ src/Fcg.Usuarios.Application/Features/Queries/ObterUsuarioPorId/ObterUsuarioPorIdQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using Fcg.Usuarios.Application.Dtos;
+using MediatR;
+
+namespace Fcg.Usuarios.Application.Features.Queries.ObterUsuarioPorId
+{
+    public class ObterUsuarioPorIdQueryHandler : IRequestHandler<ObterUsuarioPorIdQuery, UsuarioResponse>
+    {
+        public Task<UsuarioResponse> Handle(ObterUsuarioPorIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
