583532ca3057f4748ab3924358e525d310791fab
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:07:59 -0300

    refactor(application): refatora CadastrarUsuarioCommand para record

--- src/Fcg.Usuarios.Application/Features/Commands/CadastrarUsuario/CadastrarUsuarioCommand.cs
+++ src/Fcg.Usuarios.Application/Features/Commands/CadastrarUsuario/CadastrarUsuarioCommand.cs
@@ -1,13 +1,12 @@
-﻿using Fcg.Usuarios.Application.Dtos;
+using Fcg.Usuarios.Application.Dtos;
 using MediatR;
 
 namespace Fcg.Usuarios.Application.Features.Commands.CadastrarUsuario
 {
-    public class CadastrarUsuarioCommand : IRequest<LoginResponse>
-    {
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public string ConfirmacaoSenha { get; set; }
-    }
+    public record CadastrarUsuarioCommand(
+        string Nome,
+        string Email,
+        string Senha,
+        string ConfirmacaoSenha
+    ) : IRequest<LoginResponse>;
 }
