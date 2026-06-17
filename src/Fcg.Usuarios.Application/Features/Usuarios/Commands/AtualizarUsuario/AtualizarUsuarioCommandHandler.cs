30f897cdb00b74618549d44d4a556b432885d2ef
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 15:58:23 -0300

    feat: implement handler for AtualizarUsuarioCommand

--- src/Fcg.Usuarios.Application/Features/Usuarios/Commands/AtualizarUsuario/AtualizarUsuarioCommandHandler.cs
+++ src/Fcg.Usuarios.Application/Features/Usuarios/Commands/AtualizarUsuario/AtualizarUsuarioCommandHandler.cs
@@ -1,12 +1,56 @@
-﻿using MediatR;
+﻿using Fcg.Usuarios.Domain.Common;
+using Fcg.Usuarios.Domain.Common.Exceptions;
+using Fcg.Usuarios.Domain.Common.Interfaces;
+using Fcg.Usuarios.Domain.Constants;
+using Fcg.Usuarios.Domain.Repositories.Interfaces;
+using Fcg.Usuarios.Domain.ValueObjects;
+using MediatR;
 
 namespace Fcg.Usuarios.Application.Features.Usuarios.Commands.AtualizarUsuario
 {
     public class AtualizarUsuarioCommandHandler : IRequestHandler<AtualizarUsuarioCommand, UsuarioResponse>
     {
-        public Task<UsuarioResponse> Handle(AtualizarUsuarioCommand request, CancellationToken cancellationToken)
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        public AtualizarUsuarioCommandHandler(IUsuarioRepository usuarioRepository, IPasswordHasher passwordHasher)
         {
-            throw new NotImplementedException();
+            _usuarioRepository = usuarioRepository; 
+            _passwordHasher = passwordHasher;
+        }
+        public async Task<UsuarioResponse> Handle(AtualizarUsuarioCommand request, CancellationToken cancellationToken)
+        {
+            var usuario = await _usuarioRepository.ObterPorId(request.UsuarioId);
+            if (usuario == null) throw new DomainException(MensagensDominio.UsuarioNaoEncontrado);
+
+            if (await _usuarioRepository.VerificaNomeCadastradoParaAlteracao(request.UsuarioId, request.NomeUsuario))
+            {                
+                throw new DomainException(MensagensDominio.NomeUsuarioJaCadastrado);
+            }
+
+            if (request.SenhaUsuario != request.ConfirmacaoSenha)
+                throw new DomainException(MensagensDominio.UsuarioSenhaConfirmacaoDiferente);
+
+            AssertionConcern.AssertArgumentLength(request.SenhaUsuario, 8, 60, MensagensDominio.SenhaTamanhoInvalido);
+            AssertionConcern.AssertArgumentPasswordStrenght(request.SenhaUsuario, MensagensDominio.UsuarioSenhaFraca);
+
+            var hashSenha = _passwordHasher.HashPassword(request.SenhaUsuario);
+
+            var novaSenhaCriptografa = new Senha(hashSenha);
+
+            var novoUsuarioVO = new Nome(request.NomeUsuario);
+
+            usuario.Atualizar(novoUsuarioVO, novaSenhaCriptografa);
+
+            _usuarioRepository.Atualizar(usuario);
+
+            await _usuarioRepository.SaveChanges();
+
+            return new UsuarioResponse
+            {
+                Nome = usuario.NomeUsuario.Valor,
+                Email = usuario.EmailUsuario.Valor,
+                PerfilUsuario = usuario.Perfil
+            };
         }
     }
 }
