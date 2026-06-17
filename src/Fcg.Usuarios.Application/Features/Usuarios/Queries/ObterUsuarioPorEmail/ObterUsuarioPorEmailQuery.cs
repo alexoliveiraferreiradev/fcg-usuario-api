ed9c41bbbdb3780e37bde6276607c6e99c84cc90
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 18:10:21 -0300

    refactor: delete unused ObterUsuarioPorEmail query and handler

--- src/Fcg.Usuarios.Application/Features/Usuarios/Queries/ObterUsuarioPorEmail/ObterUsuarioPorEmailQuery.cs
+++
@@ -1,7 +0,0 @@
-using Fcg.Usuarios.Application.Features.Usuarios;
-using MediatR;
-
-namespace Fcg.Usuarios.Application.Features.Usuarios.Queries.ObterUsuarioPorEmail
-{
-    public record ObterUsuarioPorEmailQuery : IRequest<UsuarioResponse>;
-}
