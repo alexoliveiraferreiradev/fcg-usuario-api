014c86a0e6cae6324b5626cf9ea1b299aa379690
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:08:01 -0300

    refactor(application): refatora RebaixarUsuarioParaJogadorCommand para record

--- src/Fcg.Usuarios.Application/Features/Commands/RebaixarUsuarioParaJogador/RebaixarUsuarioParaJogadorCommand.cs
+++ src/Fcg.Usuarios.Application/Features/Commands/RebaixarUsuarioParaJogador/RebaixarUsuarioParaJogadorCommand.cs
@@ -1,11 +1,10 @@
-﻿using Fcg.Usuarios.Application.Dtos;
+using Fcg.Usuarios.Application.Dtos;
 using MediatR;
 
 namespace Fcg.Usuarios.Application.Features.Commands.RebaixarUsuarioParaJogador
 {
-    public class RebaixarUsuarioParaJogadorCommand : IRequest<UsuarioResponse>
-    {
-        public Guid Id { get; set; }
-        public Guid IdOperador { get; set; }
-    }
+    public record RebaixarUsuarioParaJogadorCommand(
+        Guid Id,
+        Guid IdOperador
+    ) : IRequest<UsuarioResponse>;
 }
