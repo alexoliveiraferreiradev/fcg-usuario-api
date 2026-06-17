64acbc1d28b38fa85d15cf9c6d348a1747a47f30
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:08:03 -0300

    feat(application): adiciona ObterUsuarioPorEmailQueryHandler

---
+++ src/Fcg.Usuarios.Application/Features/Queries/ObterUsuarioPorEmail/ObterUsuarioPorEmailQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using Fcg.Usuarios.Application.Dtos;
+using MediatR;
+
+namespace Fcg.Usuarios.Application.Features.Queries.ObterUsuarioPorEmail
+{
+    public class ObterUsuarioPorEmailQueryHandler : IRequestHandler<ObterUsuarioPorEmailQuery, UsuarioResponse>
+    {
+        public Task<UsuarioResponse> Handle(ObterUsuarioPorEmailQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
