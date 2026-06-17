e43a4d4977095f3dcf24b7fa608dcfbaac954ba1
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:08:02 -0300

    feat(application): adiciona ObterTodosUsuariosQueryHandler

---
+++ src/Fcg.Usuarios.Application/Features/Queries/ObterTodosUsuarios/ObterTodosUsuariosQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using Fcg.Usuarios.Application.Dtos;
+using MediatR;
+
+namespace Fcg.Usuarios.Application.Features.Queries.ObterTodosUsuarios
+{
+    public class ObterTodosUsuariosQueryHandler : IRequestHandler<ObterTodosUsuariosQuery, IEnumerable<UsuarioResponse>>
+    {
+        public Task<IEnumerable<UsuarioResponse>> Handle(ObterTodosUsuariosQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
