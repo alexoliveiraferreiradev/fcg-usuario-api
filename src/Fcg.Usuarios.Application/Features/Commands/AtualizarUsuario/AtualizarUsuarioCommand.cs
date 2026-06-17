f282998c1cd9108048cf78421d3f5abab6ec491d
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:07:58 -0300

    refactor(application): refatora AtualizarUsuarioCommand para record

--- src/Fcg.Usuarios.Application/Features/Commands/AtualizarUsuario/AtualizarUsuarioCommand.cs
+++ src/Fcg.Usuarios.Application/Features/Commands/AtualizarUsuario/AtualizarUsuarioCommand.cs
@@ -1,13 +1,12 @@
-﻿using Fcg.Usuarios.Application.Dtos;
+using Fcg.Usuarios.Application.Dtos;
 using MediatR;
 
 namespace Fcg.Usuarios.Application.Features.Commands.AtualizarUsuario
 {
-    public class AtualizarUsuarioCommand : IRequest<UsuarioResponse>
-    {
-        public Guid UsuarioId { get; set; }
-        public string NomeUsuario { get; set; }
-        public string SenhaUsuario { get; set; }
-        public string ConfirmacaoSenha { get; set; }
-    }
+    public record AtualizarUsuarioCommand(
+        Guid UsuarioId,
+        string NomeUsuario,
+        string SenhaUsuario,
+        string ConfirmacaoSenha
+    ) : IRequest<UsuarioResponse>;
 }
