4f8f25185f86cb6e863cdaf993dab0f57f5f8e61
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-17 12:07:59 -0300

    refactor(application): refatora AutenticarUsuarioCommand para record

--- src/Fcg.Usuarios.Application/Features/Commands/AutenticarUsuario/AutenticarUsuarioCommand.cs
+++ src/Fcg.Usuarios.Application/Features/Commands/AutenticarUsuario/AutenticarUsuarioCommand.cs
@@ -1,11 +1,10 @@
-﻿using Fcg.Usuarios.Application.Dtos;
+using Fcg.Usuarios.Application.Dtos;
 using MediatR;
 
 namespace Fcg.Usuarios.Application.Features.Commands.AutenticarUsuario
 {
-    public class AutenticarUsuarioCommand : IRequest<LoginResponse>
-    {        
-        public string Email { get; set; }
-        public string Senha { get; set; }
-    }
+    public record AutenticarUsuarioCommand(
+        string Email,
+        string Senha
+    ) : IRequest<LoginResponse>;
 }
