1e75092aa303e39ef1ff424f9061328c9ac2a980
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-18 11:32:32 -0300

    refactor: limpar construtor parcial, validacoes e parametros redundantes na camada de dominio

--- src/Fcg.Usuarios.Domain/Common/Interfaces/IAggregateRoot.cs
+++ src/Fcg.Usuarios.Domain/Common/Interfaces/IAggregateRoot.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fcg.Usuarios.Domain.Common.Interfaces
+﻿namespace Fcg.Usuarios.Domain.Common.Interfaces
 {
     public interface IAggregateRoot
     {    }
