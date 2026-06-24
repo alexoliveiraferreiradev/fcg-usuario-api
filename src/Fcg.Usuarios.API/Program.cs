c3dd740abfa6aa9998999467ee0885a8f17efb07
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-24 16:01:14 -0300

    feat: configurar MassTransit, RabbitMq e banco de dados na API

--- src/Fcg.Usuarios.API/Program.cs
+++ src/Fcg.Usuarios.API/Program.cs
@@ -1,10 +1,43 @@
+using Fcg.Usuarios.Domain.Repositories.Interfaces;
+using Fcg.Usuarios.Infrastructure.Persistance;
+using Fcg.Usuarios.Infrastructure.Repository;
+using MassTransit;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<UsuarioDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UsuarioConnection"));
+});
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddEntityFrameworkOutbox<UsuarioDbContext>(o =>
+    {
+        o.UseSqlServer();
+    });
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMq"));
+    });
+});
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+
+builder.Services.AddScoped<UsuarioDbContext>();
+builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
 var app = builder.Build();
 
@@ -13,6 +46,12 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fiap Cloud Games");
+});
 
 app.UseHttpsRedirection();
 
