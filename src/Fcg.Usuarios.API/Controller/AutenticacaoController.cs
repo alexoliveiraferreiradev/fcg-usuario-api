d0531b0816dd0b319a056904fe88711d59ada3fc
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-24 16:01:26 -0300

    feat: adicionar controller de usuarios na API

---
+++ src/Fcg.Usuarios.API/Controller/AutenticacaoController.cs
@@ -0,0 +1,29 @@
+﻿using Fcg.Usuarios.Application.Features.Usuarios.Commands.AutenticarUsuario;
+using Fcg.Usuarios.Application.Features.Usuarios.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fcg.Usuario.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AutenticacaoController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AutenticacaoController(IMediator mediator)
+        {
+            _mediator = mediator;   
+        }
+
+        [HttpPost("cadastrar")]
+        [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<LoginResponse>> Cadastrar(AutenticarUsuarioCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return Ok(response);    
+        }
+    }
+}
