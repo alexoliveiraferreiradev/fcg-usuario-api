544e2f3381f30b50f04c4bf19778f74510ff322b
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-06-24 23:28:33 -0300

    feat(users): remove AutenticacaoController e cria NovaContaController para cadastro

--- src/Fcg.Usuarios.API/Controller/NovaContaController.cs
+++ src/Fcg.Usuarios.API/Controller/NovaContaController.cs
@@ -1,4 +1,4 @@
-﻿using Fcg.Usuarios.Application.Features.Usuarios.Commands.AutenticarUsuario;
+﻿using Fcg.Usuarios.Application.Features.Usuarios.Commands.CadastrarUsuario;
 using Fcg.Usuarios.Application.Features.Usuarios.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -9,10 +9,10 @@ namespace Fcg.Usuario.API.Controller
     [Route("api/[controller]")]
     [ApiController]
     [AllowAnonymous]
-    public class AutenticacaoController : ControllerBase
+    public class NovaContaController : ControllerBase
     {
-        private readonly IMediator _mediator;
-        public AutenticacaoController(IMediator mediator)
+        private readonly ISender _mediator;
+        public NovaContaController(ISender mediator)
         {
             _mediator = mediator;   
         }
@@ -20,9 +20,10 @@ namespace Fcg.Usuario.API.Controller
         [HttpPost("cadastrar")]
         [ProducesResponseType(typeof(LoginResponse), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<LoginResponse>> Cadastrar(AutenticarUsuarioCommand command)
+        public async Task<ActionResult<LoginResponse>> Cadastrar(CadastrarUsuarioCommand command, 
+            CancellationToken cancellationToken)
         {
-            var response = await _mediator.Send(command);
+            var response = await _mediator.Send(command, cancellationToken);
             return Ok(response);    
         }
     }
