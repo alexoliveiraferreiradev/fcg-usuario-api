6dcff648119a4bf20ce194d72edf29f7d2f684df
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-03 11:19:01 -0300

    docs: adicionar sumario XML nos Commands da aplicacao

--- src/Fcg.Users.Application/Features/Users/Commands/AuthenticateUser/AuthenticateUserCommand.cs
+++ src/Fcg.Users.Application/Features/Users/Commands/AuthenticateUser/AuthenticateUserCommand.cs
@@ -1,8 +1,13 @@
-﻿using Fcg.Users.Application.Features.Users.Responses;
+using Fcg.Users.Application.Features.Users.Responses;
 using MediatR;
 
 namespace Fcg.Users.Application.Features.Users.Commands.AuthenticateUser
 {
+    /// <summary>
+    /// Comando enviado para autenticar um usuário no sistema.
+    /// </summary>
+    /// <param name="Email">Endereço de e-mail de acesso cadastrado.</param>
+    /// <param name="Password">Senha correspondente cadastrada.</param>
     public record AuthenticateUserCommand(
         string Email,
         string Password
