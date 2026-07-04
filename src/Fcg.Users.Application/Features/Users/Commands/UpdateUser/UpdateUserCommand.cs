26a24f23f6fa45b079bc1c5746b36c99fb24245b
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-04 17:43:27 -0300

    refactor(users): adiciona DefaultValue às propriedades de UpdateUserCommand

--- src/Fcg.Users.Application/Features/Users/Commands/UpdateUser/UpdateUserCommand.cs
+++ src/Fcg.Users.Application/Features/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -1,19 +1,19 @@
 using Fcg.Users.Application.Features.Users.Responses;
 using MediatR;
+using System.ComponentModel;
 
 namespace Fcg.Users.Application.Features.Users.Commands.UpdateUser
 {
     /// <summary>
     /// Comando enviado pelo próprio usuário autenticado para atualizar seus dados cadastrais.
     /// </summary>
-    /// <param name="UserId">Identificador único (GUID) do usuário logado.</param>
     /// <param name="Name">Novo nome completo a ser atualizado.</param>
     /// <param name="Password">Nova senha a ser cadastrada.</param>
     /// <param name="ConfirmPassword">Confirmação idêntica da nova senha.</param>
     public record UpdateUserCommand(
-        Guid UserId,
-        string Name,
-        string Password,
-        string ConfirmPassword
+        [property: DefaultValue("00000000-0000-0000-0000-000000000000")] Guid UserId,
+        [property: DefaultValue("nome do usuário")] string Name,
+        [property: DefaultValue("senha do usuário")] string Password,
+        [property: DefaultValue("senha do usuário")] string ConfirmPassword
     ) : IRequest<UserResponse>;
 }
