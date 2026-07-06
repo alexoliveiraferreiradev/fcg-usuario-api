6ae6c9ddedb490369e50cc72b1a51a5c342efb04
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-05 23:53:39 -0300

    refactor(application): criar interface de consulta IAdminQueryRepository

---
+++ src/Fcg.Users.Application/Common/Interfaces/IAdminQueryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Fcg.Users.Application.Features.Users.Responses;
+
+namespace Fcg.Users.Application.Common.Interfaces
+{
+    public interface IAdminQueryRepository
+    {
+        Task<IEnumerable<UserResponse>> GetAllUsersAsync(CancellationToken cancellation);
+        Task<UserResponse?> GetUserByIdAsync(Guid userId, CancellationToken cancellation);
+    }
+}
