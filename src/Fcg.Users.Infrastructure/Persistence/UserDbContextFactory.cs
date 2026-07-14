48202a6620b1c32e934094ceaf648ba8d501b0a3
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-07-14 00:12:48 -0300

    feat(infrastructure): adicionar UserDbContextFactory para design-time tooling do Entity Framework

---
+++ src/Fcg.Users.Infrastructure/Persistence/UserDbContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Fcg.Core.Abstractions.Common.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Fcg.Users.Infrastructure.Persistence
+{
+    public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
+    {
+        public UserDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+            var host = configuration["DatabaseSettings:Host"] ?? "localhost";
+            var port = configuration["DatabaseSettings:Port"] ?? "1433";
+            var dbName = configuration["DatabaseSettings:DatabaseName"];
+            var user = configuration["DatabaseSettings:Username"];
+            var password = configuration["DatabaseSettings:Password"];
+
+            if (string.IsNullOrEmpty(host))
+                throw new DomainException("Faltam variáveis de ambiente de Banco de Dados para rodar a migração.");
+
+            var connectionString = $"Data Source={host},{port};Initial Catalog={dbName};User ID={user};Password={password};TrustServerCertificate=True";
+
+            var builder = new DbContextOptionsBuilder<UserDbContext>();
+            builder.UseSqlServer(connectionString);
+
+            return new UserDbContext(builder.Options);
+        }
+    }
+}
