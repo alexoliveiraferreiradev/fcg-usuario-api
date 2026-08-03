6aed1118272074dc8c235138a8b6de131195a24a
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-08-03 14:34:48 -0300

    refactor: extrair configuracao de servicos da Application para ApplicationServiceCollectionExtensions

---
+++ src/Fcg.Users.Application/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Fcg.Users.Application.Features.Users.Commands.RegisterUser;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fcg.Users.Application.Extensions
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(RegisterUserCommand).Assembly);
+
+            });
+            services.AddValidatorsFromAssembly(typeof(RegisterUserCommand).Assembly);
+            return services;
+        }
+    }
+}
