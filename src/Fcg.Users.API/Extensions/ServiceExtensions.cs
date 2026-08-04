8fa0dca7996455c7738f2e238a530e4e733a76c7
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-08-04 09:45:49 -0300

    refactor(api): remove usings não utilizados

--- src/Fcg.Users.API/Extensions/ServiceExtensions.cs
+++ src/Fcg.Users.API/Extensions/ServiceExtensions.cs
@@ -1,28 +1,5 @@
-using Dapper;
-using Fcg.Core.Abstractions.Interfaces;
-using Fcg.Core.WebApi.Security;
-using Fcg.Users.Application.Common.Interfaces;
 using Fcg.Users.Application.Extensions;
-using Fcg.Users.Application.Features.Users.Commands.RegisterUser;
-using Fcg.Users.Domain.Common.Interfaces;
-using Fcg.Users.Domain.Repositories.Interfaces;
 using Fcg.Users.Infrastructure.Extensions;
-using Fcg.Users.Infrastructure.MessageBroker;
-using Fcg.Users.Infrastructure.Persistence;
-using Fcg.Users.Infrastructure.Queries;
-using Fcg.Users.Infrastructure.Queries.DapperHandlers;
-using Fcg.Users.Infrastructure.Repository;
-using Fcg.Users.Infrastructure.Security;
-using FluentValidation;
-using MassTransit;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Serilog;
-using System.Data;
-using System.Text;
 
 namespace Fcg.User.API.Extensions
 {
